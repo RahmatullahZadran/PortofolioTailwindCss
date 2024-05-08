3e34ec994974144cb541783848e0c28fd82ce59a
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-08 21:49:22 +0100

    updated nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from './components/navbat.js';
+import Midle from './components/midle.js';
 import './App.css';
+import Navbar from './components/navbar.js';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Midle />
     </div>
   );
 }
