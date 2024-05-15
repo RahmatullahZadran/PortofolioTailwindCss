3dc2a42110c3e3b67925b4340f3bfe68081e33f7
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-15 21:49:27 +0100

    adjusted icons on the project

--- src/components/quickview.js
+++ src/components/quickview.js
@@ -49,7 +49,7 @@ function ProjectQuickView({ project, isOpen, onClose }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl transition-all sm:my-8 sm:max-w-xlg">
                 <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     {project.image && (
@@ -62,7 +62,7 @@ function ProjectQuickView({ project, isOpen, onClose }) {
                         {project.name}
                       </Dialog.Title>
                       <div className="mt-2">
-                        <p className="text-sm text-gray-300">{project.info}</p>
+                   
                         
                         <div className="mt-4">
                         <p className="text-sm text-gray-300">{project.description}</p>
@@ -88,7 +88,7 @@ function ProjectQuickView({ project, isOpen, onClose }) {
                                 onMouseLeave={() => handleGithubMouseLeave(project.id)}
                                 onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
                               >
-                                <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ${githubScaled[project.id] ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/github%20(1).png?alt=media&token=eb89d9c4-1c80-4be8-8124-fc6dceb7add8" alt="GitHub Icon" />
+                                <img className={`h-8 w-8 sm:h-5 sm:w-5 animate-jumpy md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8 ${githubScaled[project.id] ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/github%20(1).png?alt=media&token=eb89d9c4-1c80-4be8-8124-fc6dceb7add8" alt="GitHub Icon" />
                               </a>
                               <a
                                 href={project.url}
@@ -98,7 +98,7 @@ function ProjectQuickView({ project, isOpen, onClose }) {
                                 onMouseLeave={() => handleUrlMouseLeave(project.id)}
                                 onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
                               >
-                                <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ${urlScaled[project.id] ? 'scale-110' : ''}`} src={project.url ? "https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db" : "https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/chain.png?alt=media&token=e6853ad5-9c7b-40e3-94b0-6e4a4231e37d"} alt="URL Icon" />
+                                <img className={`h-8 w-8 sm:h-5 sm:w-5 animate-jumpy md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8 ${urlScaled[project.id] ? 'scale-110' : ''}`} src={project.url ? "https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db" : "https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/chain.png?alt=media&token=e6853ad5-9c7b-40e3-94b0-6e4a4231e37d"} alt="URL Icon" />
                               </a>
                             </div>
                           </div>
