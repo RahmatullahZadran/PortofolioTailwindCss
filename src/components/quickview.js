1f94ba539405f1cd4e62a19794ca0c943e3bfddd
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-15 21:14:32 +0100

    adjusted quickview

--- src/components/quickview.js
+++ src/components/quickview.js
@@ -31,7 +31,7 @@ function ProjectQuickView({ project, isOpen, onClose }) {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="bg-gray-800  px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     {project.image && (
                       <div className="mx-auto flex-shrink-0  flex items-center justify-center h-60 w-40 overflow-hidden sm:mx-0 sm:h-60  sm:w-60">
@@ -66,13 +66,14 @@ function ProjectQuickView({ project, isOpen, onClose }) {
                         )}
                         {project.githubUrl && (
                           <div className="mt-4">
+                            <text className="text-sm font-medium text-white">Links</text>
                             <a
                               href={project.githubUrl}
                               target="_blank"
                               rel="noopener noreferrer"
                               className="text-indigo-600 hover:text-indigo-900"
                             >
-                              <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db" alt="URL Icon" />
+                              <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/github%20(1).png?alt=media&token=eb89d9c4-1c80-4be8-8124-fc6dceb7add8" alt="URL Icon" />
                             </a>
                           </div>
                         )}
@@ -80,10 +81,10 @@ function ProjectQuickView({ project, isOpen, onClose }) {
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse">
+                <div className="bg-gray-900 px-4 py-3 sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={onClose}
                   >
                     Close
