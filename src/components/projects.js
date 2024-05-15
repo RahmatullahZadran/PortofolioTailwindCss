2b9d87bf5b1d19137f63dd8a2fa2e2a59629b69b
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-15 01:22:42 +0100

    adjusted errors

--- src/components/projects.js
+++ src/components/projects.js
@@ -24,8 +24,8 @@ export default function Example() {
   const [projects, setProjects] = useState([]);
   const [githubScaled, setGithubScaled] = useState({});
   const [urlScaled, setUrlScaled] = useState({});
-  const [selectedProject, setSelectedProject] = useState(null);  // State for the selected project
-  const [isQuickViewOpen, setIsQuickViewOpen] = useState(false); // State for the quick view
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [isQuickViewOpen, setIsQuickViewOpen] = useState(false);
 
   useEffect(() => {
     const projectsRef = db.ref('Projects');
@@ -58,7 +58,7 @@ export default function Example() {
     setUrlScaled((prev) => ({ ...prev, [id]: false }));
   };
 
-  const handleProjectClick = (project) => {
+  const handleProjectImageClick = (project) => {
     setSelectedProject(project);
     setIsQuickViewOpen(true);
   };
@@ -77,9 +77,14 @@ export default function Example() {
 
         <ul role="list" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 sm:gap-y-3 xl:col-span-5">
           {projects.map((project) => (
-            <li key={project.id} onClick={() => handleProjectClick(project)}>
-              <div className="flex flex-col items-start cursor-pointer">
-                <img className="h-60 w-80 sm:h-72 sm:w-80 rounded object-cover animate-slideInTop" src={project.image} alt={project.id} />
+            <li key={project.id}>
+              <div className="flex flex-col items-start">
+                <img
+                  className="h-60 w-80 sm:h-72 sm:w-80 rounded object-cover animate-slideInTop cursor-pointer"
+                  src={project.image}
+                  alt={project.id}
+                  onClick={() => handleProjectImageClick(project)}
+                />
                 <div>
                   <h3 className="text-lg animate-slideInLeft font-semibold text-white">{project.name}</h3>
                   <p className="text-base text-gray-400">{project.description}</p>
@@ -90,7 +95,7 @@ export default function Example() {
                       rel="noopener noreferrer"
                       onMouseEnter={() => handleGithubMouseEnter(project.id)}
                       onMouseLeave={() => handleGithubMouseLeave(project.id)}
-                      onClick={(e) => e.stopPropagation()} // Prevent triggering the project click event
+                      onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
                     >
                       <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ${githubScaled[project.id] ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/github%20(1).png?alt=media&token=eb89d9c4-1c80-4be8-8124-fc6dceb7add8" alt="GitHub Icon" />
                     </a>
@@ -101,7 +106,7 @@ export default function Example() {
                         rel="noopener noreferrer"
                         onMouseEnter={() => handleUrlMouseEnter(project.id)}
                         onMouseLeave={() => handleUrlMouseLeave(project.id)}
-                        onClick={(e) => e.stopPropagation()} // Prevent triggering the project click event
+                        onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
                       >
                         <img className={`h-4 w-4 sm:h-5 sm:w-5 animate-jumpy md:h-5 md:w-5 lg:h-5 lg:w-5 xl:h-6 xl:w-6 ${urlScaled[project.id] ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db" alt="URL Icon" />
                       </a>
