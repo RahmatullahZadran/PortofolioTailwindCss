1f006a418865e38b45ce52b60999be0189daa238
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-16 22:54:03 +0100

    adjusted footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,8 @@
 import React from "react";
 const Footer = () => {
   return (
-    
-
-      <div className="flex flex-col max-w-[1900px] px-2 py-4 mx-auto justify-between bg-gray-800  sm:flex-row text-center text-gray-500">
-        <p className="py-4">2022 Workflow, LLC. All rights reserved</p>
-      
+      <div className="flex flex-col max-w-[1900px] px-2  py-1 mx-auto justify-between bg-gray-800  sm:flex-row text-center text-gray-500">
+        <p className="py-4 animate-jumpy text-white mx-auto">2022 Workflow, LLC. All rights reserved</p>
       </div>
    
   );
