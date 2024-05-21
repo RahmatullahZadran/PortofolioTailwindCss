064628c2406092789dcd34782605d51c3841ac6e
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-21 23:27:11 +0100

    adjusted front page

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ const Footer = () => {
   return (
       <div className="flex flex-col max-w-[1900px] px-2  py-1 mx-auto justify-between bg-gray-800  sm:flex-row text-center text-gray-500">
         <p className="py-1 animate-jumpy text-white  mx-auto">Rahamtullah Zadran Portpfolio</p>
+        <p className="py-1 animate-jumpy text-white mx-auto">In Progress</p>
       </div>
    
   );
--- src/components/midle.js
+++ src/components/midle.js
@@ -1,14 +1,14 @@
 export default function Example() {
   return (
-    <section className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
+    <section className="relative isolate overflow-hidden bg-gray-900 px-6 py-10 sm:py-10 lg:px-8">
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-slate-300 opacity-20 shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
         <figure className="mt-1" >
         <figcaption className="mt-0">
             <img
-              className=" rounded-full mx-auto h-50 w-50 animate-slideInBottom"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              className=" rounded-full mx-auto w-40 h-40 sm:w-60 sm:h-60 xl:w-70 xl:h-70 animate-slideInBottom"
+              src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/oic2.jpg?alt=media&token=1c115c0e-7cfd-46dc-80d9-57b5d674ce50"
               alt=""
             />
             <div className="mt-1 flex items-center justify-center space-x-3 text-base">
