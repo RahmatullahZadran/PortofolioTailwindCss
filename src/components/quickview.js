6f7a3cbf7367f04b0c47fc77a35678016cfe24e5
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-22 18:24:49 +0100

    added quickview adjustment

--- src/components/quickview.js
+++ src/components/quickview.js
@@ -33,17 +33,16 @@ function ProjectQuickView({ project, isOpen, onClose, skills }) {
       <Dialog as="div" className="relative z-10" onClose={onClose}>
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-6">
-            <Transition.Child
-            >
+            <Transition.Child>
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl">
-                <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    {project.image && (
-                      <div className="mx-auto flex-shrink-0 flex items-center justify-center h-50 w-80 overflow-hidden sm:h-60 sm:w-90 ">
-                        <img src={project.image} alt={project.name} className="h-full w-full" />
-                      </div>
-                    )}
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <div className="bg-gray-800">
+                  {project.image && (
+                    <div className="flex items-center justify-center h-50 w-90 overflow-hidden sm:h-60 sm:w-90 xl:h-80 xl:w-90">
+                      <img src={project.image} alt={project.name} className="h-full w-full" />
+                    </div>
+                  )}
+                  <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="text-center sm:text-left">
                       <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
                         {project.name}
                       </Dialog.Title>
@@ -79,43 +78,41 @@ function ProjectQuickView({ project, isOpen, onClose, skills }) {
                               <img className={`h-8 w-8 sm:h-5 sm:w-5 animate-jumpy md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8 ${githubScaled[project.id] ? 'scale-110' : ''}`} src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/github%20(1).png?alt=media&token=eb89d9c4-1c80-4be8-8124-fc6dceb7add8" alt="GitHub Icon" />
                             </a>
                             {project.url ? (
-  <a
-    href={project.url}
-    target="_blank"
-    rel="noopener noreferrer"
-    onMouseEnter={() => handleUrlMouseEnter(project.id)}
-    onMouseLeave={() => handleUrlMouseLeave(project.id)}
-    onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
-  >
-    <img
-      className={`h-8 w-8 xsm:h-3 sm:h-5 sm:w-5 animate-jumpy md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8 ${urlScaled[project.id] ? 'scale-110' : ''}`}
-      src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db"
-      alt="URL Icon"
-    />
-  </a>
-) : (
-  <img
-    className="h-8 w-8 xsm:h-3 sm:h-5 sm:w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8"
-    src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/chain.png?alt=media&token=e6853ad5-9c7b-40e3-94b0-6e4a4231e37d"
-    alt="URL Icon"
-  />
-)}
-
+                              <a
+                                href={project.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                onMouseEnter={() => handleUrlMouseEnter(project.id)}
+                                onMouseLeave={() => handleUrlMouseLeave(project.id)}
+                                onClick={(e) => e.stopPropagation()} // Prevent triggering the project image click event
+                              >
+                                <img
+                                  className={`h-8 w-8 xsm:h-3 sm:h-5 sm:w-5 animate-jumpy md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8 ${urlScaled[project.id] ? 'scale-110' : ''}`}
+                                  src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/link%20(1).png?alt=media&token=6f3eebed-d3f3-4d0d-b1f0-74c6f22c71db"
+                                  alt="URL Icon"
+                                />
+                              </a>
+                            ) : (
+                              <img
+                                className="h-8 w-8 xsm:h-3 sm:h-5 sm:w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-8 xl:w-8"
+                                src="https://firebasestorage.googleapis.com/v0/b/protofolio-44836.appspot.com/o/chain.png?alt=media&token=e6853ad5-9c7b-40e3-94b0-6e4a4231e37d"
+                                alt="URL Icon"
+                              />
+                            )}
                           </div>
                         </div>
                       )}
                     </div>
                   </div>
-                </div>
-                <div className="bg-gray-900 px-4 py-3 sm:flex sm:flex-row-reverse">
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring
-                    -2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={onClose}
-                  >
-                    Close
-                  </button>
+                  <div className="bg-gray-900 px-4 py-3 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={onClose}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
