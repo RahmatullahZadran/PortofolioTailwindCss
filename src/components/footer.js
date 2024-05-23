e558170137c7283ea91aabf5e284519fd5c21133
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-23 22:14:28 +0100

    fixed footer bg colour

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const { isDarkMode } = useContext(DarkModeContext);
 
   return (
-    <div className={`flex flex-col max-w-[1900px] px-2 py-1 mx-auto justify-between ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black'} sm:flex-row text-center`}>
+    <div className={`flex flex-col max-w-[1900px] px-2 py-1 mx-auto justify-between ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'} sm:flex-row text-center`}>
       <p className="py-1 animate-jumpy mx-auto">Rahmatullah Zadran Portfolio</p>
       <p className="py-1 animate-jumpy mx-auto">In Progress</p>
     </div>
