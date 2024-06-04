12fedbc07d0e7e0b6f89423812e3e3a64cbcbec6
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-06-04 23:22:06 +0100

    added pic and updated json file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,36 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // Enable dark mode using the 'class' strategy
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {
       keyframes: {
-        slideInLeft: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        slideInRight: {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-        slideInTop: {
+        'slide-from-top': {
           '0%': { transform: 'translateY(-100%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
-        slideInBottom: {
+        'slide-from-bottom': {
           '0%': { transform: 'translateY(100%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+        'slide-from-left': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
         },
-        jumpy: {
-          '0%': { transform: 'translateY(0)' },
-          '25%': { transform: 'translateY(-20px)' },
-          '50%': { transform: 'translateY(0)' },
-          '75%': { transform: 'translateY(-10px)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        grow: {
-          '0%': { transform: 'scale(1)' },
-          '100%': { transform: 'scale(1.1)' },
+        'slide-from-right': {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
         },
       },
       animation: {
-        slideInLeft: 'slideInLeft 0.3s ease-out',
-        slideInRight: 'slideInRight 0.3s ease-out',
-        slideInTop: 'slideInTop 0.3s ease-out',
-        slideInBottom: 'slideInBottom 0.3s ease-out',
-        fadeIn: 'fadeIn 0.3s ease-out',
-        jumpy: 'jumpy 0.5s ease-out',
-        grow: 'grow 0.3s ease-out',
+        'slide-from-top': 'slide-from-top 1s ease-out',
+        'slide-from-bottom': 'slide-from-bottom 1s ease-out',
+        'slide-from-left': 'slide-from-left 1s ease-out',
+        'slide-from-right': 'slide-from-right 1s ease-out',
       },
     },
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-};
+  plugins: [],
+}
+ 
